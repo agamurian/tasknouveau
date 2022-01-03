6d73ccecb5a9715a039f289627e961f2d609ff35
Agamura Ian <agamurian@gmail.com>
2022-01-03 06:22:31 +0300

    added jsonify

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.get("/")
 def hello_world():
-    return {result:'succes'}
+    return jsonify({"result":"succes"})
