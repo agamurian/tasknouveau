a80849bc0a04948191d0a37c9ed0219b7abcd997
Agamura Ian <agamurian@gmail.com>
2022-01-03 08:17:29 +0300

    added all the crud

--- backend/app.py
+++ backend/app.py
@@ -12,14 +12,14 @@ class Note(db.Model):
     text = db.Column(db.String(10000), nullable=False) 
 
 
+# CRUD
+
 @app.get("/notes")
-def get_notes():
+def get_all_notes():
     response_object = {'status': 'success'}
     notes = Note.query.all()
-    print(notes)
     response_list = []
     for note in notes:
-        print(note)
         response_list.append({
             'id': note.id,
             'name': note.name,
@@ -28,24 +28,53 @@ def get_notes():
     response_object['notes'] = response_list
     return jsonify(response_object)
 
+
 @app.post("/note")
-def new_note():
+def create_note():
     response_object = {'status':'success'}
     note_data = request.get_json()
     generated_id = secrets.token_hex(4)
-    print(generated_id)
     name = note_data.get('name')
     text = note_data.get('text')
     note = Note(id=generated_id, name=name, text=text)
     db.session.add(note)
     db.session.commit()
-    print(note)
     return jsonify(response_object)
 
 
-@app.get("/note/{id}")
-def note(id):
-    pass
+@app.get("/note/<id>")
+def get_note(id):
+    response_object = {'status': 'success'}
+    note = Note.query.get_or_404(id)
+    response_list = {
+        'id': note.id,
+        'name': note.name,
+        'text': note.text,
+    }
+    response_object['note'] = response_list
+    return jsonify(response_object)
+
+
+@app.post("/note/<id>")
+def update_note(id):
+    response_object = {'status': 'success'}
+    note = Note.query.get_or_404(id)
+    note_data = request.get_json()
+    note.name = note_data.get('name')
+    note.text = note_data.get('text')
+    db.session.add(note)
+    db.session.commit()
+    return jsonify(response_object)
+
+
+@app.delete("/note/<id>")
+def delete_note(id):
+    response_object = {'status': 'success'}
+    note = Note.query.get_or_404(id)
+    db.session.delete(note)
+    db.session.commit()
+    return jsonify(response_object)
+
 
 @app.get("/")
 def hello_world():
