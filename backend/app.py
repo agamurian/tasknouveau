72b42dcd884d515dc54a080545b22486c2fa2220
Agamura Ian <agamurian@gmail.com>
2022-01-03 06:58:48 +0300

    added sqlite database

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,15 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+db = SQLAlchemy(app)
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False) 
+    text = db.Column(db.String(10000), nullable=False) 
 
 @app.get("/")
 def hello_world():
-    return jsonify({"result":"succes"})
+    return jsonify({"result":"success"})
