c054aa8130996761ee06c32f08a872fe58415f35
Agamura Ian <agamurian@gmail.com>
2022-01-03 07:16:31 +0300

    added notes model and get notes route

--- backend/app.py
+++ backend/app.py
@@ -10,6 +10,25 @@ class Note(db.Model):
     name = db.Column(db.String(120), nullable=False) 
     text = db.Column(db.String(10000), nullable=False) 
 
+
+@app.get("/notes")
+def notes():
+    response_object = {'status': 'success'}
+    notes = Note.query
+    response_list = []
+    for note in notes:
+        response_list.append({
+            'id': notes.id,
+            'name': notes.name,
+            'text': notes.text,
+        })
+    response_object['notes'] = response_list
+    return jsonify(response_object)
+
+@app.get("/note/{id}")
+def note(id):
+    pass
+
 @app.get("/")
 def hello_world():
     return jsonify({"result":"success"})
