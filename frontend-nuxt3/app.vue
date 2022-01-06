9398d4e6b232efa7e99679a8f049741db7e7f79e
Agamura Ian <agamurian@gmail.com>
2022-01-07 00:47:29 +0300

    fixed sveltekit folder

--- frontend-nuxt3/app.vue
+++ frontend-nuxt3/app.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
     <p>something</p>
+    <Note />
   </div>
 </template>
+
+<script>
+  import Note from "./components/Note.vue"
+  
+  export default {
+  components: {
+    Note,
+  }
+}
+  
+</script>
---
+++ frontend-nuxt3/components/Note.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="p-8 bg-gray">
+    <h2>note</h2>
+    <p>text of note</p>
+  </div>
+</template>
+
+<script>
+  
+</script>
